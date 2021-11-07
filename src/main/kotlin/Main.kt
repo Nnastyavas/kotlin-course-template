ac43abdd91e6dea51d40e8df4d14e190b13251df
nnastyavas <nnastyavas@gmail.com>
2021-11-07 22:00:08 +0300

    lab1 and unit tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,11 +3,16 @@ import lab1.*
 fun main() {
 
     val string = """ 
-        Скажи ка дядя, ведь не даром Москва спаленная пожаром Французу отдана. 
-        Ведь были ж схватки боевые, да говорят "Еще какие". Не даром помнит вся Россия про день Бородина.
+        Скажи-ка, дядя, ведь не даром
+        Москва, спаленная пожаром,
+        Французу отдана?
+        Ведь были ж схватки боевые,
+        Да, говорят, еще какие!
+        Недаром помнит вся Россия
+        Про день Бородина!
     """.trimIndent()
     println("Выравнивание по центру:\n" + alignText(string, Alignment.CENTER, 10))
-    println("\nВыравнивание по левому краю:\n" + alignText(string, Alignment.LEFT, 10))
-    println("\nВыравнивание по правому краю:\n" + alignText(string, Alignment.RIGHT, 10))
-    println("\nСлово:\n" + alignText("Аргумент", Alignment.RIGHT, 3))
+    println("\nВыравнивание по левому краю:\n" + alignText(string, Alignment.LEFT, 20))
+    println("\nВыравнивание по правому краю:\n" + alignText(string, Alignment.RIGHT, 15))
+    println("\nСлово:\n" + alignText("Аргумент", Alignment.LEFT, 3))
 }
