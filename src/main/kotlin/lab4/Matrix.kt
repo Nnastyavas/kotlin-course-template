d97a47feb168a23fef997870400e77529a7018b8
nnastyavas <nnastyavas@gmail.com>
2021-11-13 17:47:39 +0300

    lab 4

---
+++ src/main/kotlin/lab4/Matrix.kt
@@ -0,0 +1,198 @@
+package lab4
+
+class Matrix(var matrix: Array<Array<Double>>) {
+
+    private val columns: Int = matrix[0].size //колонки
+    private val rows: Int = matrix.size //строки
+
+    // Просмотр размерностей матрицы
+    private fun getColumns(): Int = columns
+    private fun getRows(): Int = rows
+
+    fun size(): String {
+        return ("[${getRows()}, ${getColumns()}]")
+    }
+
+    //Инициализация матрицы
+    init {
+        if (matrix.isEmpty() || matrix[0].isEmpty())
+            throw Exception ("Matrix is empty")
+        matrix.forEach {
+            if (it.size != getColumns())
+                throw Exception ("Size of the rows must be the same")
+        }
+    }
+
+    //Просмотр значения элемента в позиции (i,j) и изменения этого элемента
+    operator fun set(i: Int, j: Int, value: Double) {
+        matrix[i][j] = value
+    }
+
+    operator fun get(i: Int, j: Int): Double {
+        return matrix[i][j]
+    }
+
+    // Операции + - * (создание новой матрицы)
+    operator fun plus(other: Matrix): Matrix {
+        if (getRows() == other.getRows() && getColumns() == other.getColumns()) {
+            val otherMatrix: Array<Array<Double>> = Array(getRows()) { Array(getColumns()) {0.0} }
+            for (i in 0 until rows) {
+                for (j in 0 until columns) {
+                    otherMatrix[i][j] += matrix[i][j] + other.matrix[i][j]
+                }
+            }
+            return Matrix(otherMatrix)
+        }
+        throw Exception("Wrong size of matrix")
+    }
+
+    operator fun minus(other: Matrix): Matrix {
+        if (getRows() == other.getRows() && getColumns() == other.getColumns()) {
+            val otherMatrix: Array<Array<Double>> = Array(getRows()) { Array(getColumns()) {0.0} }
+            for (i in 0 until getRows()) {
+                for (j in 0 until getColumns()) {
+                    otherMatrix[i][j] += matrix[i][j] - other.matrix[i][j]
+                }
+            }
+            return Matrix(otherMatrix)
+        }
+        throw Exception("Wrong size of matrix")
+    }
+
+    operator fun times(other: Matrix): Matrix {
+        if (getColumns() == other.getRows()) {
+            val otherMatrix: Array<Array<Double>> = Array(getRows()) { Array(other.getColumns()) { 0.0 } }
+            for (i in 0 until getRows()) {
+                for (j in 0 until other.getColumns()) {
+                    for (k: Int in 0 until getColumns()) {
+                        otherMatrix[i][j] += matrix[i][k] * other.matrix[k][j]
+                    }
+                }
+            }
+            return Matrix(otherMatrix)
+        } else throw Exception("Columns are not equal to rows")
+    }
+
+    // Операции += -= *= (модификация операций слева)
+    operator fun plusAssign(other: Matrix) {
+        if (getColumns() == other.getColumns() && getRows() == other.getRows()) {
+            for (i in 0 until getRows()) {
+                for (j in 0 until getColumns()) {
+                    matrix[i][j] += other[i, j]
+                }
+            }
+        } else throw Exception("Wrong size of matrix")
+    }
+
+    operator fun minusAssign(other: Matrix) {
+        if (getColumns() == other.getColumns() && getRows() == other.getRows()) {
+            for (i in 0 until getRows()) {
+                for (j in 0 until getColumns()) {
+                    matrix[i][j] -= other[i, j]
+                }
+            }
+        } else throw Exception("Wrong size of matrix")
+    }
+
+    operator fun timesAssign(other: Matrix) {
+        if (getColumns() == other.getRows()) {
+            val tempMatrix: Array<Array<Double>> = Array(getRows()) { Array(other.getColumns()) { 0.0 } }
+            for (i in 0 until getRows()) {
+                for (j in 0 until other.getColumns()) {
+                    for (k: Int in 0 until getColumns()) {
+                        tempMatrix[i][j] += matrix[i][k] * other.matrix[k][j]
+                    }
+                }
+            }
+            matrix = tempMatrix
+        } else throw Exception("Columns are not equal to rows")
+    }
+
+    //Умножение и деление на скаляр (создание новой матрицы)
+    operator fun times(scalar: Double): Matrix {
+        val otherMatrix: Array<Array<Double>> = Array(getRows()) { Array(getColumns()) { 0.0 } }
+        for (i in 0 until getRows()) {
+            for (j in 0 until getColumns())
+                otherMatrix[i][j] += (matrix[i][j] * scalar)
+        }
+        return Matrix(otherMatrix)
+    }
+
+    operator fun div(scalar: Double): Matrix {
+        val otherMatrix: Array<Array<Double>> = Array(getRows()) { Array(getColumns()) { 0.0 } }
+        if (scalar == 0.0)
+            throw Exception ("Division by zero is not possible")
+        for (i in 0 until getRows()) {
+            for (j in 0 until getColumns()) {
+                otherMatrix[i][j] += (matrix[i][j] / scalar)
+            }
+        }
+        return Matrix(otherMatrix)
+    }
+
+    //Умножение и деление на скаляр (Модификация матрицы слева)
+    operator fun timesAssign(scalar: Double) {
+        for (i in 0 until getRows()) {
+            for (j in 0 until getColumns())
+                matrix[i][j] = (matrix[i][j] * scalar)
+        }
+    }
+
+    operator fun divAssign(scalar: Double) {
+        if (scalar == 0.0)
+            throw Exception ("Division by zero is not possible")
+        for (i in 0 until getRows()) {
+            for (j in 0 until getColumns()) {
+                matrix[i][j] = (matrix[i][j] / scalar)
+            }
+        }
+    }
+
+    // Операторы унарного минуса и унарного плюса
+    operator fun unaryMinus(): Matrix {
+        for (i in 0 until getRows()) {
+            for (j in 0 until getColumns()) {
+                if (matrix[i][j] != 0.0)
+                    matrix[i][j] = -matrix [i][j]
+            }
+        }
+        return Matrix(matrix)
+    }
+
+    operator fun unaryPlus(): Matrix {
+        return this
+    }
+
+    //Сравнение двух матриц ==
+    override operator fun equals (other: Any?): Boolean {
+       val temp: Matrix = other as Matrix
+        if (temp.getColumns() != getColumns() || temp.getRows() != getRows())
+            return false
+        for (i in 0 until getRows())
+            for (j in 0 until getColumns())
+                if (matrix[i][j] != temp[i,j])
+                    return false
+        return true
+    }
+
+    //Вывод состояния матрицы в строку toString
+    override fun toString(): String {
+        var str = ""
+        for (element in matrix) {
+            for (j in 0 until matrix[0].size) {
+                str+="["
+                str += element[j].toString()
+                str += "]"
+            }
+            str += System.lineSeparator()
+        }
+        return str
+    }
+
+    override fun hashCode(): Int {    //сгенериррован средой разработки
+        var result = matrix.contentDeepHashCode()
+        result = 31 * result + columns
+        result = 31 * result + rows
+        return result
+    }
+}
---
+++ src/test/kotlin/lab4/MatrixTest.kt
@@ -0,0 +1,345 @@
+package lab4
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
+internal class MatrixTest {
+
+    @Test
+    fun exception_EmptyMatrix(){
+        try {
+            Matrix(arrayOf(arrayOf()))
+        }catch(e: Exception){
+            assertEquals("Matrix is empty", e.message)
+        }
+    }
+
+    @Test
+    fun exception_SizeRows(){
+        try {
+            Matrix(arrayOf(
+                arrayOf(1.0,2.0),
+                arrayOf(4.0),
+                arrayOf(7.0,8.0)))
+        }catch(e: Exception){
+            assertEquals("Size of the rows must be the same", e.message)
+        }
+    }
+    @Test
+    fun exception_PlusOrMinus(){
+        try {
+            val matrix = Matrix(arrayOf(
+                arrayOf(1.0,2.0),
+                arrayOf(4.0,1.0),
+                arrayOf(7.0,8.0)))
+            val matrix2 = Matrix(arrayOf(
+                arrayOf(1.0,2.0),
+                arrayOf(4.0,1.0)))
+            matrix + matrix2
+        }catch(e: Exception){
+            assertEquals("Wrong size of matrix", e.message)
+        }
+    }
+    @Test
+    fun exception_Times(){
+        try {
+            val matrix = Matrix(arrayOf(
+                arrayOf(1.0,2.0,9.0),
+                arrayOf(4.0,7.0,8.0)))
+            matrix * matrix
+        }catch(e: Exception){
+            assertEquals("Columns are not equal to rows", e.message)
+        }
+    }
+
+    @Test
+    fun exception_MinusAssign(){
+        try {
+            val matrix = Matrix(arrayOf(
+                arrayOf(1.0,2.0),
+                arrayOf(4.0,1.0),
+                arrayOf(7.0,8.0)))
+            val matrix2 = Matrix(arrayOf(
+                arrayOf(1.0,2.0),
+                arrayOf(4.0,1.0)))
+            matrix -= matrix2
+        }catch(e: Exception){
+            assertEquals("Wrong size of matrix", e.message)
+        }
+    }
+    @Test
+    fun exception_TimesAssign(){
+        try {
+            val matrix = Matrix(arrayOf(
+                arrayOf(1.0,2.0,9.0),
+                arrayOf(4.0,7.0,8.0)))
+            matrix *= matrix
+        }catch(e: Exception){
+            assertEquals("Columns are not equal to rows", e.message)
+        }
+    }
+
+    @Test
+    fun exception_DivScalar(){
+        try {
+            val matrix = Matrix(arrayOf(
+                arrayOf(1.0,2.0),
+                arrayOf(4.0,1.0),
+                arrayOf(7.0,8.0)))
+            val scalar = 0.0
+            matrix / scalar
+        }catch(e: Exception){
+            assertEquals("Division by zero is not possible", e.message)
+        }
+    }
+
+    @Test
+    fun exception_DivAssignScalar(){
+        try {
+            val matrix = Matrix(arrayOf(
+                arrayOf(1.0,2.0),
+                arrayOf(4.0,1.0),
+                arrayOf(7.0,8.0)))
+            val scalar = 0.0
+            matrix /= scalar
+        }catch(e: Exception){
+            assertEquals("Division by zero is not possible", e.message)
+        }
+    }
+
+    @Test
+    fun size() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(1.0,2.0),
+            arrayOf(4.0,1.0),
+            arrayOf(7.0,8.0)))
+        assertEquals("[3, 2]",matrix.size())
+    }
+
+    @Test
+    fun set() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(1.0,2.0),
+            arrayOf(4.0,1.0),
+            arrayOf(7.0,8.0)))
+        matrix[0, 0] = 4.0
+        assertEquals(4.0, matrix[0, 0])
+    }
+
+    @Test
+    fun get() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(1.0,2.0),
+            arrayOf(4.0,1.0),
+            arrayOf(7.0,8.0)))
+        assertEquals(8.0, matrix[2,1])
+    }
+
+    @Test
+    fun plus() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(1.0,2.0),
+            arrayOf(4.0,1.0),
+            arrayOf(7.0,8.0)))
+        val matrix2 = Matrix(arrayOf(
+            arrayOf(2.0,2.0),
+            arrayOf(4.0,1.0),
+            arrayOf(1.0,5.0)))
+        val expected = "[3.0][4.0]" + System.lineSeparator() +
+                       "[8.0][2.0]" + System.lineSeparator() +
+                       "[8.0][13.0]" + System.lineSeparator()
+        assertEquals(expected, (matrix + matrix2).toString())
+        assertEquals(1.0, matrix[0, 0])
+        assertNotEquals(3.0, matrix[0, 0])
+    }
+
+    @Test
+    fun plusAssign() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(1.0,2.0),
+            arrayOf(4.0,1.0),
+            arrayOf(7.0,8.0)))
+        val matrix2 = Matrix(arrayOf(
+            arrayOf(2.0,2.0),
+            arrayOf(4.0,1.0),
+            arrayOf(1.0,5.0)))
+        matrix += matrix2
+        val expected = "[3.0][4.0]" + System.lineSeparator() +
+                       "[8.0][2.0]" + System.lineSeparator() +
+                       "[8.0][13.0]" + System.lineSeparator()
+        assertEquals(expected, matrix.toString())
+        assertEquals(3.0, matrix[0, 0])
+        assertNotEquals(1.0, matrix[0, 0])
+    }
+
+    @Test
+    fun minus() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(1.0,-2.0),
+            arrayOf(-4.0,1.0),
+            arrayOf(7.0,-8.0)))
+        val matrix2 = Matrix(arrayOf(
+            arrayOf(2.0,2.0),
+            arrayOf(4.0,1.0),
+            arrayOf(1.0,5.0)))
+        val expected = "[-1.0][-4.0]" + System.lineSeparator() +
+                       "[-8.0][0.0]" + System.lineSeparator() +
+                       "[6.0][-13.0]" + System.lineSeparator()
+        assertEquals(expected, (matrix - matrix2).toString())
+        assertEquals(1.0, matrix[0, 0])
+        assertNotEquals(-1.0, matrix[0, 0])
+    }
+
+    @Test
+    fun minusAssign() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(1.0,-2.0),
+            arrayOf(-4.0,1.0),
+            arrayOf(7.0,-8.0)))
+        val matrix2 = Matrix(arrayOf(
+            arrayOf(2.0,2.0),
+            arrayOf(4.0,1.0),
+            arrayOf(1.0,5.0)))
+        matrix -= matrix2
+        val expected = "[-1.0][-4.0]" + System.lineSeparator() +
+                       "[-8.0][0.0]" + System.lineSeparator() +
+                       "[6.0][-13.0]" + System.lineSeparator()
+        assertEquals(expected, matrix.toString())
+        assertEquals(-1.0, matrix[0, 0])
+        assertNotEquals(1.0, matrix[0, 0])
+    }
+
+    @Test
+    fun times() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(1.0,2.0),
+            arrayOf(4.0,5.0),
+            arrayOf(7.0,8.0)))
+        val matrix2 = Matrix(arrayOf
+            (arrayOf(1.0,2.0,3.0),
+            arrayOf(4.0,5.0,6.0)))
+        val expected = "[9.0][12.0][15.0]" + System.lineSeparator() +
+                       "[24.0][33.0][42.0]" + System.lineSeparator() +
+                       "[39.0][54.0][69.0]" + System.lineSeparator()
+        assertEquals(expected, (matrix * matrix2).toString())
+        assertEquals(1.0, matrix[0, 0])
+        assertNotEquals(9.0, matrix[0, 0])
+    }
+
+    @Test
+    fun timesAssign() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(1.0,2.0),
+            arrayOf(4.0,5.0),
+            arrayOf(7.0,8.0)))
+        val matrix2 = Matrix(arrayOf
+            (arrayOf(1.0,2.0,3.0),
+            arrayOf(4.0,5.0,6.0)))
+        matrix *= matrix2
+        val expected = "[9.0][12.0][15.0]" + System.lineSeparator() +
+                       "[24.0][33.0][42.0]" + System.lineSeparator() +
+                       "[39.0][54.0][69.0]" + System.lineSeparator()
+        assertEquals(expected, matrix.toString())
+        assertEquals(9.0, matrix[0, 0])
+        assertNotEquals(1.0, matrix[0, 0])
+    }
+
+    @Test
+    fun timesScalar() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(3.0,5.0),
+            arrayOf(4.0,2.0)))
+        val scalar = 4.0
+        val expected = "[12.0][20.0]" + System.lineSeparator() +
+                       "[16.0][8.0]" + System.lineSeparator()
+        assertEquals(expected, (matrix * scalar).toString())
+        assertEquals(3.0, matrix[0, 0])
+        assertNotEquals(12.0, matrix[0, 0])
+    }
+
+    @Test
+    fun timesAssignScalar() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(3.0,5.0),
+            arrayOf(4.0,2.0)))
+        val scalar = 4.0
+        matrix *=scalar
+        val expected = "[12.0][20.0]" + System.lineSeparator() +
+                       "[16.0][8.0]" + System.lineSeparator()
+        assertEquals(expected, matrix.toString())
+        assertEquals(12.0, matrix[0, 0])
+        assertNotEquals(3.0, matrix[0, 0])
+    }
+
+    @Test
+    fun divScalar() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(3.0,5.0),
+            arrayOf(4.0,2.0)))
+        val scalar = 2.0
+        val expected = "[1.5][2.5]" + System.lineSeparator() +
+                       "[2.0][1.0]" + System.lineSeparator()
+        assertEquals(expected, (matrix / scalar).toString())
+        assertEquals(3.0, matrix[0, 0])
+        assertNotEquals(1.5, matrix[0, 0])
+    }
+
+    @Test
+    fun divAssignScalar() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(3.0,5.0),
+            arrayOf(4.0,2.0)))
+        val scalar = 2.0
+        matrix /= scalar
+        val expected = "[1.5][2.5]" + System.lineSeparator() +
+                       "[2.0][1.0]" + System.lineSeparator()
+        assertEquals(expected, matrix.toString())
+        assertEquals(1.5, matrix[0, 0])
+        assertNotEquals(3, matrix[0, 0])
+    }
+
+    @Test
+    operator fun unaryMinus() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(3.0,-5.0),
+            arrayOf(-4.0,2.0)))
+        val expected = "[-3.0][5.0]" + System.lineSeparator() +
+                       "[4.0][-2.0]" + System.lineSeparator()
+        assertEquals(expected, (-matrix).toString())
+        assertEquals(-3.0, matrix[0, 0])
+        assertNotEquals(3.0, matrix[0, 0])
+    }
+
+    @Test
+    operator fun unaryPlus() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(3.0,-5.0),
+            arrayOf(-4.0,2.0)))
+        val expected = "[3.0][-5.0]" + System.lineSeparator() +
+                       "[-4.0][2.0]" + System.lineSeparator()
+        assertEquals(expected, (+matrix).toString())
+        assertEquals(3.0, matrix[0, 0])
+    }
+
+    @Test
+    fun testEquals() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(3.0,-5.0),
+            arrayOf(-4.0,2.0)))
+        val matrix2 = Matrix(arrayOf(
+            arrayOf(3.0,-5.0),
+            arrayOf(-4.0,2.0)))
+        assertEquals(true, matrix == matrix2)
+    }
+
+    @Test
+    fun testNotEquals() {
+        val matrix = Matrix(arrayOf(
+            arrayOf(1.0,-5.0),
+            arrayOf(-4.0,2.0)))
+        val matrix2 = Matrix(arrayOf(
+            arrayOf(3.0,-5.0),
+            arrayOf(-4.0,2.0)))
+        assertEquals(false, matrix == matrix2)
+    }
+}
